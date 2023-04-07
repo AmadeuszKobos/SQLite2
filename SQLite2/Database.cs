dfb79ac5af5fcb9d3851d4aa2d2d53400f35bdec
Amadeusz <Acer@LAPTOP-DCKI4AHO>
2023-04-07 12:15:47 +0200

    Dodaj pliki projektów.

---
+++ SQLite2/Database.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using System.IO;
+
+namespace SQLite2
+{
+    internal class Database
+    {
+        public SQLiteConnection conn; //connection to database
+
+        public Database()
+        {
+            conn = new SQLiteConnection("Data Source=database.sqlite3");
+
+            if (!File.Exists("./database.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("database.sqlite3"); //if file named database.sqlite3 doesn't exist, we create one
+                Console.WriteLine("Created database file");
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if(conn.State != ConnectionState.Open)
+            { 
+                conn.Open();
+            }
+
+        }
+        public void CloseConnection()
+        {
+            if (conn.State != ConnectionState.Closed)
+            {
+                conn.Close();
+            }
+        }
+
+    }
+}
---
+++ SQLite2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLite2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string BLUE = Console.IsOutputRedirected ? "" : "\x1b[94m";
+            string NORMAL      = Console.IsOutputRedirected ? "" : "\x1b[39m";
+
+            Database databaseObj = new Database();
+            
+
+
+            /* ADDING ROW TO DB */
+            
+            string query = "INSERT INTO 'Table' ('Name','Surname','Age') VALUES (@name, @surname, @age)";
+            SQLiteCommand myInsert = new SQLiteCommand(query, databaseObj.conn);
+
+            databaseObj.OpenConnection();//connect to db
+
+            myInsert.Parameters.AddWithValue("@name", "Ronaldinho");
+            myInsert.Parameters.AddWithValue("@surname", "Gaúcho");
+            myInsert.Parameters.AddWithValue("@age", "18");
+
+            var insertResult = myInsert.ExecuteNonQuery();
+            Console.WriteLine("Rows added: {0}", insertResult);
+            
+            databaseObj.CloseConnection(); //end connection to db 
+
+            /* ADDING ROW TO DB */
+
+
+
+            /* TABLE SELECT */
+
+            string query2 = "SELECT * FROM 'Table'";
+            SQLiteCommand mySelect = new SQLiteCommand(query2, databaseObj.conn);
+
+
+            databaseObj.OpenConnection(); //connect to db
+
+            SQLiteDataReader selectResult = mySelect.ExecuteReader();
+
+
+            if (selectResult.HasRows)
+            {
+                Console.WriteLine("Selected Rows");
+                while (selectResult.Read())
+                {
+                    Console.Write($"  |  {BLUE}Name:{NORMAL} " + selectResult["Name"] + $"  |  {BLUE}Surname:{NORMAL}" + selectResult["Surname"] + $"  |  {BLUE}Age:{NORMAL} " + selectResult["Age"] + '\n');
+                }
+            }
+
+            databaseObj.CloseConnection(); //end db connection
+
+            /* TABLE SELECT */
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
